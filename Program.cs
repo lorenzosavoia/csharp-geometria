4f02a7b17064cca82da8d49225494c3a8ae93947
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-05-16 18:11:06 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Consegna: Nel progetto csharp - geometria creare le seguenti classi
+Creare una classe Rettangolo con due attributi interi: baseRettangolo e altezzaRettangolo. Aggiungere un opportuno costruttore con parametri. Aggiungere due metodi: calcolaArea e calcolaPerimetro che calcolano e restituiscono, rispettivamente, l’area e il perimetro del rettangolo.
+Poi, nel vostro programma principale (Program.cs) chiedete all’utente di inserire, da console, i valori di base e di altezza con cui istanziare un nuovo Rettangolo. Dopo averlo istanziato, stampate a video il perimetro e l’area.
+Provate ad istanziare tanti rettangoli con nome diverso e attributi diversi (baseRettangolo e altezzaRettangolo) e provate a stampare le loro proprietà ad esempio con un metodo StampaRettangolo che vi stampi il vostro rettangolo ad esempio cosi:
+—— Rettangolo1 —— base: 20 cm altezza: 10 cm Perimetro: 60 cm Area: 200 cm2
+BONUS Aggiungere alla classe Rettangolo un metodo “disegna” che disegna in console il rettangolo con le sue dimensioni, ossia tanti “—” (due trattini) orizzontali quanto è grande la sua base e tanti ‘|’ verticali quanto e grande la sua altezza.
+*/
+
+using System;
+
+namespace cshap_geometria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Quanto é alto il tuo rettangolo?");
+            int iBase = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("qual é la base del rettangolo?");
+            int iAltezza = Convert.ToInt32(Console.ReadLine());
+            Rettangolo rettangolo1 = new Rettangolo(iBase,iAltezza);
+
+            int iAreaRettangolo = Rettangolo.AreaCalc(iBase,iAltezza);
+            int iPerimetroRettangolo = Rettangolo.PerimetroCalc(iBase, iAltezza);
+
+            //richiamo la funzione stampa rettangolo
+            //nome rettangono.nome funzione
+            rettangolo1.StampaRettangolo();
+
+        }
+    }
+        
+}
---
+++ Rettangolo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cshap_geometria
+{
+    //—— Rettangolo1 —— base: 20 cm altezza: 10 cm Perimetro: 60 cm Area: 200 cm2
+    internal class Rettangolo
+    {
+        private int iBaseRettangolo;
+        private int iAltezzaRettangolo;
+
+        public Rettangolo(int iBaseRettangolo , int iAltezzaRettangolo)
+        {
+           this.iBaseRettangolo = iBaseRettangolo;
+           this.iAltezzaRettangolo = iAltezzaRettangolo;
+        }
+        public static int AreaCalc(int iBase, int iAltezza)
+        {
+            int iArea = iBase * iAltezza;
+            return iArea;
+        }
+
+        public static int PerimetroCalc(int iBase, int iAltezza)
+        {
+            int iPerimetro = (iBase * 2) + (iAltezza * 2);
+            return iPerimetro;
+        }
+        public void StampaRettangolo()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("base {0} cm", iBaseRettangolo);
+            Console.WriteLine("altezza {0} cm", iAltezzaRettangolo);
+            Console.WriteLine("perimetro {0} cm", PerimetroCalc(iBaseRettangolo,iAltezzaRettangolo));
+            Console.WriteLine("area {0}", AreaCalc(iBaseRettangolo,iAltezzaRettangolo));
+        }
+    }
+}
